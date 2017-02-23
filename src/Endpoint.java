7db974a98d2ae60ed92189d098f913f349d72505
Igork <igorkulian@gmail.com>
2017-02-23 21:01:01 +0200

    Initial commit

---
+++ src/Endpoint.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+/**
+ * Created by User on 23.02.2017.
+ */
+public class Endpoint {
+
+    int datacenter;
+    public HashMap<Integer, Long> cacheList;
+    public HashMap<Integer, Long> requestList;
+
+
+    public Endpoint(int datacenter, HashMap<Integer, Long> cacheList) {
+        this.datacenter = datacenter;
+        this.cacheList = cacheList;
+        requestList = new HashMap<>();
+    }
+
+    public void addRequest(Integer video, Long num) {
+        requestList.put(video, num);
+    }
+
+
+
+}
